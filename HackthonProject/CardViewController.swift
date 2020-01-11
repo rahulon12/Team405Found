b72519a663c803a0d545242745d42046e63d7598
Rahul N <rahulon@icloud.com>
2020-01-11 15:42:10 +0530

    - 14 flowers

---
+++ HackthonProject/CardViewController.swift
@@ -0,0 +1,25 @@
+//
+//  CardViewController.swift
+//  HackthonProject
+//
+//  Created by Rahul Narayanan on 1/11/20.
+//  Copyright © 2020 Rahul Narayanan. All rights reserved.
+//
+
+import UIKit
+
+class CardViewController: UIViewController {
+
+    @IBOutlet var handleArea: UIView!
+    @IBOutlet var itemLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+
+}
--- HackthonProject/ViewController.swift
+++ HackthonProject/ViewController.swift
@@ -13,18 +13,153 @@ import AVKit
 
 class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
-    @IBOutlet var itemLabel: UILabel!
-    @IBOutlet var visualView: UIVisualEffectView!
-    
     let captureSession = AVCaptureSession()
+    
+    enum CardState {
+        case expanded
+        case collapsed
+    }
+    
+    var cardViewController:CardViewController!
+    var visualEffectView:UIVisualEffectView!
+    
+    let cardHeight:CGFloat = 350
+    let cardHandleAreaHeight:CGFloat = 65
+    
+    var cardVisible = false
+    var nextState:CardState {
+        return cardVisible ? .collapsed : .expanded
+    }
+    
+    var runningAnimations = [UIViewPropertyAnimator]()
+    var animationProgressWhenInterrupted:CGFloat = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         //self.view.bringSubviewToFront(visualView)
         self.setupCaptureSession()
-        self.view.bringSubviewToFront(visualView)
+        self.setupCard()
     }
+    
+    func setupCard() {
+        visualEffectView = UIVisualEffectView()
+            visualEffectView.frame = self.view.frame
+            self.view.addSubview(visualEffectView)
+            
+            cardViewController = CardViewController(nibName:"CardViewController", bundle:nil)
+            self.addChild(cardViewController)
+            self.view.addSubview(cardViewController.view)
+            
+            cardViewController.view.frame = CGRect(x: 0, y: self.view.frame.height - cardHandleAreaHeight, width: self.view.bounds.width, height: cardHeight)
+            
+            cardViewController.view.clipsToBounds = true
+            
+            let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.handleCardTap(recognzier:)))
+            let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(ViewController.handleCardPan(recognizer:)))
+            
+            cardViewController.handleArea.addGestureRecognizer(tapGestureRecognizer)
+            cardViewController.handleArea.addGestureRecognizer(panGestureRecognizer)
+            
+
+    }
+    
+    @objc
+       func handleCardTap(recognzier:UITapGestureRecognizer) {
+           switch recognzier.state {
+           case .ended:
+               animateTransitionIfNeeded(state: nextState, duration: 0.9)
+           default:
+               break
+           }
+       }
+       
+       @objc
+       func handleCardPan (recognizer:UIPanGestureRecognizer) {
+           switch recognizer.state {
+           case .began:
+               startInteractiveTransition(state: nextState, duration: 0.9)
+           case .changed:
+               let translation = recognizer.translation(in: self.cardViewController.handleArea)
+               var fractionComplete = translation.y / cardHeight
+               fractionComplete = cardVisible ? fractionComplete : -fractionComplete
+               updateInteractiveTransition(fractionCompleted: fractionComplete)
+           case .ended:
+               continueInteractiveTransition()
+           default:
+               break
+           }
+           
+       }
+       
+       func animateTransitionIfNeeded (state:CardState, duration:TimeInterval) {
+           if runningAnimations.isEmpty {
+               let frameAnimator = UIViewPropertyAnimator(duration: duration, dampingRatio: 1) {
+                   switch state {
+                   case .expanded:
+                       self.cardViewController.view.frame.origin.y = self.view.frame.height - self.cardHeight
+                   case .collapsed:
+                       self.cardViewController.view.frame.origin.y = self.view.frame.height - self.cardHandleAreaHeight
+                   }
+               }
+               
+               frameAnimator.addCompletion { _ in
+                   self.cardVisible = !self.cardVisible
+                   self.runningAnimations.removeAll()
+               }
+               
+               frameAnimator.startAnimation()
+               runningAnimations.append(frameAnimator)
+               
+               
+               let cornerRadiusAnimator = UIViewPropertyAnimator(duration: duration, curve: .linear) {
+                   switch state {
+                   case .expanded:
+                       self.cardViewController.view.layer.cornerRadius = 12
+                   case .collapsed:
+                       self.cardViewController.view.layer.cornerRadius = 0
+                   }
+               }
+               
+               cornerRadiusAnimator.startAnimation()
+               runningAnimations.append(cornerRadiusAnimator)
+               
+               let blurAnimator = UIViewPropertyAnimator(duration: duration, dampingRatio: 1) {
+                   switch state {
+                   case .expanded:
+                       self.visualEffectView.effect = UIBlurEffect(style: .dark)
+                   case .collapsed:
+                       self.visualEffectView.effect = nil
+                   }
+               }
+               
+               blurAnimator.startAnimation()
+               runningAnimations.append(blurAnimator)
+               
+           }
+       }
+       
+       func startInteractiveTransition(state:CardState, duration:TimeInterval) {
+           if runningAnimations.isEmpty {
+               animateTransitionIfNeeded(state: state, duration: duration)
+           }
+           for animator in runningAnimations {
+               animator.pauseAnimation()
+               animationProgressWhenInterrupted = animator.fractionComplete
+           }
+       }
+       
+       func updateInteractiveTransition(fractionCompleted:CGFloat) {
+           for animator in runningAnimations {
+               animator.fractionComplete = fractionCompleted + animationProgressWhenInterrupted
+           }
+       }
+       
+       func continueInteractiveTransition (){
+           for animator in runningAnimations {
+               animator.continueAnimation(withTimingParameters: nil, durationFactor: 0)
+           }
+       }
     
     func setupCaptureSession() {
         
@@ -54,7 +189,7 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         
         guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
         
-        guard let model = try? VNCoreMLModel(for: FlowerClassifier_1().model) else { return }
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier2().model) else { return }
         let request = VNCoreMLRequest(model: model) { (finishedReq, err) in
             
             //                        print(finishedReq.results)
@@ -67,10 +202,17 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
             
             DispatchQueue.main.async {
                 if firstObservation.confidence > 0.85 {
-                    self.itemLabel.text = firstObservation.identifier
+                    self.cardViewController.itemLabel.text = firstObservation.identifier
                 } else {
-                    self.itemLabel.text = "-"
+                    self.cardViewController.itemLabel.text = "-"
                 }
+                
+//                if firstObservation.confidence > 0.95 {
+//                    self.cardViewController.itemLabel.text = firstObservation.identifier
+//                } else {
+//                    self.cardViewController.itemLabel.text = "Try a different angle"
+//                }
+                
             }
             
         }

8f52f36e8ca3ef1d6b86c85cb81193c4e7240e9e
Rahul N <rahulon@icloud.com>
2020-01-11 12:13:26 +0530

    - Working prototype 1 (5 flowers)

--- HackthonProject/ViewController.swift
+++ HackthonProject/ViewController.swift
@@ -7,12 +7,76 @@
 //
 
 import UIKit
+import CoreML
+import Vision
+import AVKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
+    @IBOutlet var itemLabel: UILabel!
+    //@IBOutlet var visualView: UIVisualEffectView!
+    
+    let captureSession = AVCaptureSession()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //self.view.bringSubviewToFront(visualView)
+        self.setupCaptureSession()
+        self.view.bringSubviewToFront(itemLabel)
+    }
+    
+    func setupCaptureSession() {
+        
+        captureSession.sessionPreset = .hd1920x1080
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else {return}
+        
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else {return}
+        
+        captureSession.addInput(input)
+        captureSession.startRunning()
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(previewLayer)
+        previewLayer.frame = view.frame
+        previewLayer.cornerRadius = 8.5
+        
+        
+        let dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        captureSession.addOutput(dataOutput)
+        
+        
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        
+        guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier_1().model) else { return }
+        let request = VNCoreMLRequest(model: model) { (finishedReq, err) in
+            
+            //                        print(finishedReq.results)
+            
+            guard let results = finishedReq.results as? [VNClassificationObservation] else { return }
+            
+            guard let firstObservation = results.first else { return }
+            
+            print(firstObservation.identifier, firstObservation.confidence)
+            
+            DispatchQueue.main.async {
+                if firstObservation.confidence > 0.85 {
+                    self.itemLabel.text = firstObservation.identifier
+                } else {
+                    self.itemLabel.text = "-"
+                }
+            }
+            
+        }
+        
+        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+        
     }
 
 
